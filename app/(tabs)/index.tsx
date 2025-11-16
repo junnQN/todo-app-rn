edf7b10ec87db20848f131ac0909bb73693c8dee
Junny <quanhong468@gmail.com>
2025-11-17 05:20:02 +0700

    Integrate Convex for todo management
    Added Convex backend integration for todos, including schema, queries, and mutations. Updated UI to support adding and deleting todos, and wrapped the app in ConvexProvider. Updated dependencies to include Convex.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,19 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 
 export default function Index() {
   const { toggleDarkMode } = useTheme();
+
+  const todos = useQuery(api.todos.getTodos);
+  console.log(todos);
+
+  const addTodo = useMutation(api.todos.addTodo);
+
+  const clearAllTodo = useMutation(api.todos.deleteAllTodos);
+
   return (
     <View style={styles.container}>
       <Text style={styles.content}>
@@ -12,6 +22,12 @@ export default function Index() {
       <Text>hi</Text>
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>toggle the mode</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => addTodo({ text: "walk the dog" })}>
+        <Text>Add a new todo</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => clearAllTodo()}>
+        <Text>Delete all todo</Text>
       </TouchableOpacity>
     </View>
   );
